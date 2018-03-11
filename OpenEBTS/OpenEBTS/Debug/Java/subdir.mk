8e24fc85ff0fe23edf5c1c84cef0f32e786e53b3
delta42 <delta42@53fe7bc9-f1be-4a37-98cb-3b35ff32843c>
2018-03-11 17:31:27 +0000

    Linux refresh project where everything is built and tested on Ubuntu 16 64-bit and CentOS 7 64-bit. Project files updated for Eclipse Oxygen.

--- OpenEBTS/OpenEBTS/Debug/Java/subdir.mk
+++ OpenEBTS/OpenEBTS/Debug/Java/subdir.mk
@@ -17,10 +17,17 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
+Java/OpenEBTSJNI.o: ../Java/OpenEBTSJNI.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -std=c11 -DUNICODE -D_DEBUG -I../. -I../.. -I../Java -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"Java/OpenEBTSJNI.d" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 Java/%.o: ../Java/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -DUNICODE -D_DEBUG -I../. -I../.. -I../Java -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -DUNICODE -D_DEBUG -I../. -I../.. -I../Java -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
